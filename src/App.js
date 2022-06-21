e155fcce20ece7f7a231937889155afd4c475eff
Adnan Ahad <adnanahad@AdnanAhad.local>
2022-06-21 10:29:29 +0530

    added navigation bar with logo and shop link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
-import HomePage from "./pages/homepage/HomePage";
-import { Routes, Route } from "react-router-dom";
+import HomePage from "./routes/homepage/HomePage";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Navigation from "./routes/navigation/Navigation";
+
+const Shop = () => {
+  return <h1>Hi I am Shop</h1>;
+};
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<HomePage />} />
+          <Route path="shop" element={<Shop />} />
+        </Route>
       </Routes>
     </div>
   );
